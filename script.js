5c0c0fbc29091552812e4f14402a303471fb83a4
Mallika Mitra <mallika.mitra@journalism.cuny.edu>
2019-02-27 17:30:58 -0500

    fixed js

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
 $(document).ready(function () {
 
 
-			$("input[max], input[min]").on("keyup", function (e) {
-					var ele = $(this),
-						min = ele.attr("min"),
-						max = ele.attr("max"),
-						type = ele.attr("type"),
-						value = ele.val(),
-						container = ele.closest("div");
+	$("input[max], input[min]").on("keyup", function (e) {
+		console.log("typed!")
+		console.log(ele, min, max, type, value, container)
+
+		var ele = $(this),
+			min = ele.attr("min"),
+			max = ele.attr("max"),
+			type = ele.attr("type"),
+			value = ele.val(),
+			container = ele.closest("div");
 
 
-					if (type === "number") {
-						if (min) {
-							min = parseFloat(min);
-						} else {
-							min = false;
-						}
+		if (type === "number") {
+			if (min) {
+				min = parseFloat(min);
+			} else {
+				min = false;
+			}
 
-						if (max) {
-							max = parseFloat(max);
-						} else {
-							max = false;
-						}
+			if (max) {
+				max = parseFloat(max);
+			} else {
+				max = false;
+			}
 
-						if (min && value < min) {
-							alert("That value is too low!");
-							ele.val("");
-						}
+			if (min && value < min) {
+				alert("That value is too low!");
+				ele.val("");
+			}
 
-						if (max && value > max) {
-							alert("That value is too high!");
-							ele.val("");
-						}
-					}
-				);
+			if (max && value > max) {
+				alert("That value is too high!");
+				ele.val("");
+			}
+		}
+	});
 
-				$("#compute").on("click", function (e) {
-					e.preventDefault();
+	$("#compute").on("click", function (e) {
+		e.preventDefault();
 
-					numCommute = parseFloat($("#commute").val());
+		numCommute = parseFloat($("#commute").val());
 
-					console.log(numCommute);
+		console.log(numCommute);
 
 
-				});
+	});
 
-			});
+});
